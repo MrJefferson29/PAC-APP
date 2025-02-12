fd95dd1faee4043e1af07fcf698b7bb96f50843c
MrJefferson29 <100526664+MrJefferson29@users.noreply.github.com>
2025-02-12 06:02:29 +0100

    Update index.js

--- backend/Routes/index.js
+++ backend/Routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const authRoute = require("./auth")
 const shoproute = require('./shop')
-const featuresRoute = require('./features')
+// const featuresRoute = require('./features')
 const userRoute = require('./user')
 const aiRoute = require('./ai');
 
@@ -11,7 +11,7 @@ const aiRoute = require('./ai');
 router.use('/auth', authRoute)
 router.use('/ai', aiRoute)
 router.use('/shop', shoproute)
-router.use('/features', featuresRoute)
+// router.use('/features', featuresRoute)
 router.use('/user', userRoute)
 
 module.exports = router;
